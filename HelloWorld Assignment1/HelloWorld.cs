31ae9acfe79cbf17cce7d931e95e87b5c670902a
Dilakshan Nathan <dilaxan.super@hotmail.com>
2016-05-14 22:28:32 -0400

    Added succesful test

---
+++ HelloWorld Assignment1/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorld_Assignment1
+{
+    [TestClass]
+    public class HelloWorld
+    {
+        [TestMethod]
+        public void HelloWorldToConsoleTestMethod()
+        {
+            //Arrange
+            string expectedResult = "Hello, World!";
+            //Act
+            string result = COMP123_S2016_Assignment1.Program.HelloWorldToConsole();
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
